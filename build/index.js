b8a33a3a7f126d8fbcc2fc079f8d4fef048ffd41
Anton <anton@adc.sh>
2019-08-12 20:01:21 +0300

    t

--- build/index.js
+++ build/index.js
@@ -1,29 +1,3 @@
-const { debuglog } = require('util');
+let bug = require('@artdeco/typescript-bug'); if (bug && bug.__esModule) bug = bug.default;
 
-const LOG = debuglog('@artdeco/typescript-consumer')
-
-/**
- * Consumer of TypeScript bug.
- * @param {Config} [config] Options for the program.
- * @param {boolean} [config.shouldRun=true] A boolean option. Default `true`.
- * @param {string} config.text A text to return.
- */
-               async function typescriptConsumer(config = {}) {
-  const {
-    shouldRun = true,
-    text,
-  } = config
-  if (!shouldRun) return
-  LOG('@artdeco/typescript-consumer called with %s', text)
-  return text
-}
-
-/* documentary types/index.xml */
-/**
- * @typedef {Object} Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
- */
-
-
-module.exports = typescriptConsumer
+bug()
